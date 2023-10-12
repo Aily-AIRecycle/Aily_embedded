f9250640efada5a8aea8429a54355acbb68c0c98
2inlee <kbs@kakao.com>
2023-10-12 11:41:04 +0900

    remove rf module

--- rail/rail.ino
+++ rail/rail.ino
@@ -1,14 +1,7 @@
-#include <SPI.h>
-#include <nRF24L01.h>
-#include <RF24.h>
-
 #define dirPin1 2
 #define stepPin1 3
 #define dirPin2 4
 #define stepPin2 5
-
-RF24 radio(7, 8);
-const byte address[6] = "00001";
 
 String cmd;
 
@@ -17,10 +10,6 @@ unsigned long t1, t2;
 void setup()
 {
   Serial.begin(9600);
-  radio.begin();
-  radio.openReadingPipe(0, address);
-  radio.setPALevel(RF24_PA_MIN);
-  radio.stopListening();
   // Declare pins as output:
   pinMode(stepPin1, OUTPUT);
   pinMode(dirPin1, OUTPUT);
@@ -38,16 +27,13 @@ void loop()
     // 일반 = 1, 플라스틱 = 2, 캔 = 3
     if (cmd == "1") // 일반
     {
-      delay(9000); // 9초 대기 중 압축(Serial신호)
-
-      const char rdcmd = 't';
-      radio.write(&rdcmd, sizeof(rdcmd));
+      delay(9000); // 9초 대기 중 압축
       Serial.println("throw");
-      delay(2000); // 2초 대기 중 바닥 열림(RF신호)
+      delay(2000); // 2초 대기 중 바닥 열림
     }
     else if (cmd == "2") // 플라스틱
     {
-      delay(9000); // 9초 대기 중 압축(Serial신호)
+      delay(9000); // 9초 대기 중 압축
 
       t1 = millis();
       digitalWrite(dirPin1, HIGH);
@@ -60,9 +46,8 @@ void loop()
           break;
       }
 
-      const char rdcmd = 't';
-      radio.write(&rdcmd, sizeof(rdcmd));
-      delay(2000); // 2초 대기 중 바닥 열림(RF신호)
+      Serial.println("throw");
+      delay(2000); // 2초 대기 중 바닥 열림
 
       t1 = millis();
       digitalWrite(dirPin1, LOW);
@@ -77,7 +62,7 @@ void loop()
     }
     else if (cmd == "3") // 캔
     {
-      delay(9000); // 9초 대기 중 압축(Serial신호)
+      delay(9000); // 9초 대기 중 압축
 
       t1 = millis();
       digitalWrite(dirPin1, HIGH);
@@ -90,9 +75,8 @@ void loop()
           break;
       }
 
-      const char rdcmd = 't';
-      radio.write(&rdcmd, sizeof(rdcmd));
-      delay(2000); // 2초 대기 중 바닥 열림(RF신호)
+      Serial.println("throw");
+      delay(2000); // 2초 대기 중 바닥 열림
 
       t1 = millis();
       digitalWrite(dirPin1, LOW);
